775a0560a3aa144951c2fffcd165783cb9e5e72e
taek2222 <oot309@daum.net>
2024-12-20 22:33:49 +0900

    feat: 출결 상태 저장 필드 추가
    - 출결 상태를 저장하는 필드를 추가했습니다.
    
    - 첫 생성될 시, 결석 처리 되도록 설계했습니다.

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -1,5 +1,7 @@
 package attendance.domain;
 
+import static attendance.domain.AttendanceStatus.ABSENCE;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -7,17 +9,20 @@ import java.time.LocalTime;
 public class Attendance {
 
     private LocalDateTime dateTime;
+    private AttendanceStatus status;
 
     public Attendance(final LocalDateTime dateTime) {
         this.dateTime = dateTime;
-    }
-
-    public boolean isDefault() {
-        return dateTime.toLocalTime().equals(LocalTime.MIN);
+        this.status = ABSENCE;
     }
 
     public void updateDateTime(LocalDateTime dateTime) {
         this.dateTime = dateTime;
+        this.status = AttendanceStatus.evaluateAttendanceStatus(dateTime);
+    }
+
+    public boolean isDefault() {
+        return dateTime.toLocalTime().equals(LocalTime.MIN);
     }
 
     public boolean isSameDate(LocalDate date) {
