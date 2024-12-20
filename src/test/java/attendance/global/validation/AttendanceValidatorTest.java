bb8720d53603edeab7bd901d58a1c85d97157cce
taek2222 <oot309@daum.net>
2024-12-21 01:14:56 +0900

    feat: 출석 수정 관련 예외 테스트 설계

---
+++ src/test/java/attendance/global/validation/AttendanceValidatorTest.java
@@ -0,0 +1,33 @@
+package attendance.global.validation;
+
+import static attendance.global.constant.ErrorMessage.INVALID_INPUT;
+import static attendance.global.constant.ErrorMessage.NOT_MODIFY_DAY;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AttendanceValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 35})
+    void 잘못된_날짜를_입력한_경우_예외가_발생한다(int day) {
+        // when & then
+        assertThatThrownBy(() -> AttendanceValidator.validateDay(day))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_INPUT.get());
+    }
+
+    @Test
+    void 미래_날짜를_수정한_경우_예외가_발생한다() {
+        // given
+        int futureDay = DateTimes.now().getDayOfMonth() + 1;
+
+        // when & then
+        assertThatThrownBy(() -> AttendanceValidator.validateDay(futureDay))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_MODIFY_DAY.get());
+    }
+}
