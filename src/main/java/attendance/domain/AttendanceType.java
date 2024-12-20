6a32d6f04980e4cba029ff1e28c918ea163ac296
taek2222 <oot309@daum.net>
2024-12-20 21:57:27 +0900

    feat: 출석 시간 차이 계산 구현

---
+++ src/main/java/attendance/domain/AttendanceType.java
@@ -0,0 +1,37 @@
+package attendance.domain;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+public enum AttendanceType {
+    MONDAY(List.of(1), LocalTime.of(13, 0), LocalTime.of(18, 0)),
+    WEEKDAYS(List.of(2, 3, 4, 5), LocalTime.of(10, 0), LocalTime.of(18, 0))
+    ;
+
+    private final List<Integer> dayOfWeekValues;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    AttendanceType(final List<Integer> dayOfWeekValues, final LocalTime startTime, final LocalTime endTime) {
+        this.dayOfWeekValues = dayOfWeekValues;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public static int calculateTimeBetween(LocalDateTime dateTime) {
+        AttendanceType attendanceType = findTypeByDayOfWeek(dateTime.getDayOfWeek());
+        Duration between = Duration.between(attendanceType.startTime, dateTime.toLocalTime());
+        return (int) between.toMinutes();
+    }
+
+    private static AttendanceType findTypeByDayOfWeek(final DayOfWeek dayOfWeek) {
+        return Arrays.stream(values())
+                .filter(type -> type.dayOfWeekValues.contains(dayOfWeek.getValue()))
+                .findFirst()
+                .orElseThrow();
+    }
+}
