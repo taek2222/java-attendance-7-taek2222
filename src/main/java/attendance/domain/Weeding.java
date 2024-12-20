a82e6520fe73a8764ea826ece44ffb25775dfb4d
taek2222 <oot309@daum.net>
2024-12-21 03:44:27 +0900

    feat: 제적 상태 관리 객체 생성
    - 현재 출결 상황으로 제적을 평가하는 메서드를 설계했습니다.

---
+++ src/main/java/attendance/domain/Weeding.java
@@ -0,0 +1,39 @@
+package attendance.domain;
+
+public enum Weeding {
+    SINCERITY("성실", 0),
+    WARNING("경고", 2),
+    INTERVIEW("면담", 3),
+    WEEDING("제적", 5)
+    ;
+
+    private final String name;
+    private final int threshold;
+
+    Weeding(final String name, final int threshold) {
+        this.name = name;
+        this.threshold = threshold;
+    }
+
+    public static Weeding evaluateWeeding(int perception, int absence) {
+        int totalAbsence = absence + (perception / 3);
+
+        if (totalAbsence > WEEDING.threshold) {
+            return WEEDING;
+        }
+
+        if (totalAbsence >= INTERVIEW.threshold) {
+            return INTERVIEW;
+        }
+
+        if (totalAbsence >= WARNING.threshold) {
+            return WARNING;
+        }
+
+        return SINCERITY;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
