3e9792916d12665644ce230b04a8afd93626a3ea
taek2222 <oot309@daum.net>
2024-12-20 20:52:13 +0900

    test: 캠퍼스 운영 시간 반환값 테스트

---
+++ src/test/java/attendance/domain/CampusTimeTest.java
@@ -0,0 +1,40 @@
+package attendance.domain;
+
+import java.time.LocalTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CampusTimeTest {
+
+    @ParameterizedTest(name = "시간: {0}")
+    @CsvSource({
+            "08:00",
+            "23:00",
+            "13:00",
+            "17:00"
+    })
+    void 캠퍼스_운영_시간인_경우_TRUE를_반환한다(LocalTime time) {
+        // when
+        boolean result = CampusTime.isCampusOperateTime(time);
+
+        // then
+        Assertions.assertThat(result)
+                .isTrue();
+    }
+
+    @ParameterizedTest(name = "시간: {0}")
+    @CsvSource({
+            "07:59",
+            "23:01"
+    })
+    void 캠퍼스_운영_시간이_아닌_경우_FALSE를_반환한다(LocalTime time) {
+        // when
+        boolean result = CampusTime.isCampusOperateTime(time);
+
+        // then
+        Assertions.assertThat(result)
+                .isFalse();
+    }
+
+}
