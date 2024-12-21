3971d644a8021926050adaed35063a0a05664af4
taek2222 <oot309@daum.net>
2024-12-21 22:04:07 +0900

    style(AttendanceEvaluation): 제적 판정 네이밍 변경

--- src/main/java/attendance/domain/attendance/AttendanceEvaluation.java
+++ src/main/java/attendance/domain/attendance/AttendanceEvaluation.java
@@ -6,7 +6,7 @@ public enum AttendanceEvaluation {
     SINCERITY("성실", 0),
     WARNING("경고", 2),
     INTERVIEW("면담", 3),
-    WEEDING("제적", 6)
+    EXPULSION("제적", 6)
     ;
 
     private static final int PERCEPTION_TO_ABSENCE_RATIO = 3;
