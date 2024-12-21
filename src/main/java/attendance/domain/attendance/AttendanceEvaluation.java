caa6233e77c5612e5c4cb6768c8e38903c1e2116
taek2222 <oot309@daum.net>
2024-12-21 17:16:24 +0900

    refactor(AttendanceEvaluation): 출결 상태 if 문 Stream API 변경
    - 가독성 향상을 위해 매직 넘버를 분리했습니다.
    
    - Stream API 적용으로 일관성 있는 기준치 때문에 [제적]을 6으로 변경했습니다.

--- src/main/java/attendance/domain/attendance/AttendanceEvaluation.java
+++ src/main/java/attendance/domain/attendance/AttendanceEvaluation.java
@@ -1,11 +1,15 @@
 package attendance.domain.attendance;
 
+import java.util.Arrays;
+
 public enum AttendanceEvaluation {
     SINCERITY("성실", 0),
     WARNING("경고", 2),
     INTERVIEW("면담", 3),
-    WEEDING("제적", 5)
+    WEEDING("제적", 6)
     ;
+
+    private static final int PERCEPTION_TO_ABSENCE_RATIO = 3;
 
     private final String name;
     private final int threshold;
@@ -15,22 +19,14 @@ public enum AttendanceEvaluation {
         this.threshold = threshold;
     }
 
+    // 출결 상태 판정
     public static AttendanceEvaluation evaluateAttendance(int perception, int absence) {
-        int totalAbsence = absence + (perception / 3);
-
-        if (totalAbsence > WEEDING.threshold) {
-            return WEEDING;
-        }
-
-        if (totalAbsence >= INTERVIEW.threshold) {
-            return INTERVIEW;
-        }
-
-        if (totalAbsence >= WARNING.threshold) {
-            return WARNING;
-        }
-
-        return SINCERITY;
+        int totalAbsence = absence + (perception / PERCEPTION_TO_ABSENCE_RATIO);
+        return Arrays.stream(values())
+                .sorted((e1, e2) -> Integer.compare(e2.threshold, e1.threshold)) // 제적 -> .. -> 성실 역순 정렬
+                .filter(evaluation -> totalAbsence >= evaluation.threshold) // 평가 기준치 검사
+                .findFirst()
+                .orElse(SINCERITY);
     }
 
     public String getName() {
