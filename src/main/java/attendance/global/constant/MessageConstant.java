04f7de576ec925a5a8fa1d50668e326a76c0cab5
taek2222 <oot309@daum.net>
2024-12-21 18:35:23 +0900

    style(MessageConstant): 사용 순서에 따라 정렬

--- src/main/java/attendance/global/constant/MessageConstant.java
+++ src/main/java/attendance/global/constant/MessageConstant.java
@@ -14,17 +14,17 @@ public enum MessageConstant {
     OUTPUT_ATTENDANCE_EVALUATION("%s 대상자입니다."),
 
     // Input,
-    INPUT_TIME_FOR_MODIFICATION("언제로 변경하겠습니까?"),
-    INPUT_DAY_FOR_MODIFICATION("수정하려는 날짜(일)를 입력해 주세요."),
-    INPUT_NICKNAME_FOR_MODIFICATION("출석을 수정하려는 크루의 닉네임을 입력해 주세요."),
-    INPUT_NICKNAME("닉네임을 입력해 주세요."),
-    INPUT_SCHOOL_START_TIME("등교 시간을 입력해 주세요."),
     INPUT_FUNCTION_SELECTION("""
                 1. 출석 확인
                 2. 출석 수정
                 3. 크루별 출석 기록 확인
                 4. 제적 위험자 확인
                 Q. 종료"""),
+    INPUT_NICKNAME("닉네임을 입력해 주세요."),
+    INPUT_SCHOOL_START_TIME("등교 시간을 입력해 주세요."),
+    INPUT_NICKNAME_FOR_MODIFICATION("출석을 수정하려는 크루의 닉네임을 입력해 주세요."),
+    INPUT_DAY_FOR_MODIFICATION("수정하려는 날짜(일)를 입력해 주세요."),
+    INPUT_TIME_FOR_MODIFICATION("언제로 변경하겠습니까?"),
 
     DEFAULT_TIME("--:--"),
     NEW_LINE(System.lineSeparator())
