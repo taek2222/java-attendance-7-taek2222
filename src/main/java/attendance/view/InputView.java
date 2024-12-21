33f65e58376f3f007f340586dad8dd843766643a
taek2222 <oot309@daum.net>
2024-12-21 19:17:25 +0900

    style(InputView): 사용 순서에 따라 정렬

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -14,18 +14,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String readTimeForModification() {
-        return printMessageAndReadInput(INPUT_TIME_FOR_MODIFICATION);
-    }
-
-    public int readDayForModification() {
-        String input = printMessageAndReadInput(INPUT_DAY_FOR_MODIFICATION);
-        validateIsNumeric(input);
-        return Integer.parseInt(input);
-    }
-
-    public String readNicknameForModification() {
-        return printMessageAndReadInput(INPUT_NICKNAME_FOR_MODIFICATION);
+    public String readFunctionSelection() {
+        String input = printMessageAndReadInput(INPUT_FUNCTION_SELECTION);
+        validateFunctionSelection(input);
+        return input;
     }
 
     public String readNickname() {
@@ -36,10 +28,18 @@ public class InputView {
         return printMessageAndReadInput(INPUT_SCHOOL_START_TIME);
     }
 
-    public String readFunctionSelection() {
-        String input = printMessageAndReadInput(INPUT_FUNCTION_SELECTION);
-        validateFunctionSelection(input);
-        return input;
+    public String readNicknameForModification() {
+        return printMessageAndReadInput(INPUT_NICKNAME_FOR_MODIFICATION);
+    }
+
+    public int readDayForModification() {
+        String input = printMessageAndReadInput(INPUT_DAY_FOR_MODIFICATION);
+        validateIsNumeric(input);
+        return Integer.parseInt(input);
+    }
+
+    public String readTimeForModification() {
+        return printMessageAndReadInput(INPUT_TIME_FOR_MODIFICATION);
     }
 
     private String printMessageAndReadInput(MessageConstant message) {
